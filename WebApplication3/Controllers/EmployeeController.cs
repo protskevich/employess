c8e2cc0c2bc8ed359c4e4bd4cd5fabfa9fa78ab6
Alexander Protskevich <protskevich@gmail.com>
2020-02-12 14:38:33 +0300

    Initial commit

---
+++ WebApplication3/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        static List<Employee> employees;
+
+        static EmployeeController()
+        {
+            employees = new List<Employee>();
+            employees.Add(new Employee { Id = 1, FirstName = "Alexander", LastName = "Protskevich", Age = 27 });
+        }
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var employee = employees.FirstOrDefault(p => p.Id == id);
+            if (employee != null)
+            {
+                return View(employee);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            var oldEmployee = employees.FirstOrDefault(p => p.Id == employee.Id);
+            if (oldEmployee != null)
+            {
+                oldEmployee.FirstName = employee.FirstName;
+                oldEmployee.LastName = employee.FirstName;
+                oldEmployee.Age = employee.Age;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
